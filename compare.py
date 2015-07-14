39f734d367946457bf2e93cc57a3483c9206f346
Shafeeq K <shafeeq94@gmail.com>
2015-07-14 21:51:55 +0530

    Add python&opencv solution

---
+++ compare.py
@@ -0,0 +1,57 @@
+import matplotlib.pyplot as plt
+import cv2
+import sys
+import os
+import shutil
+import requests
+
+
+url1, url2 = sys.argv[1:]
+
+print "Downloading image 1"
+response = requests.get(url1, stream=True)
+with open('img1', 'wb') as out_file:
+   shutil.copyfileobj(response.raw, out_file)
+del response
+
+print "Downloading image 2"
+response = requests.get(url2, stream=True)
+with open('img2', 'wb') as out_file:
+   shutil.copyfileobj(response.raw, out_file)
+del response
+
+image1 = cv2.imread('img1')
+image2 = cv2.imread('img2')
+
+mimage1 = cv2.cvtColor(image1, cv2.COLOR_BGR2RGB)
+mimage2 = cv2.cvtColor(image2, cv2.COLOR_BGR2RGB)
+
+hist1 = cv2.calcHist([mimage1], [0, 1, 2], None, [8, 8, 8],
+	[0, 256, 0, 256, 0, 256])
+hist1 = cv2.normalize(hist1).flatten()
+hist2 = cv2.calcHist([mimage2], [0, 1, 2], None, [8, 8, 8],
+	[0, 256, 0, 256, 0, 256])
+hist2 = cv2.normalize(hist2).flatten()
+
+
+d = cv2.compareHist(hist1, hist2, cv2.cv.CV_COMP_CORREL)
+if d < 0:
+	d = 0
+print "Similarity  = %.2f%%" %(d*100)
+
+# show the query images
+fig = plt.figure("Image1")
+ax = fig.add_subplot(1, 1, 1)
+ax.imshow(mimage1)
+plt.axis("off")
+fig = plt.figure("Image2")
+ax = fig.add_subplot(1, 1, 1)
+ax.imshow(mimage2)
+plt.axis("off")
+
+
+plt.show()
+
+
+os.remove('img1')
+os.remove('img2')
